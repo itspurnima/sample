a0017efad937758c30bacbb82f20259b901ebcc5
itspurnima <61597739+itspurnima@users.noreply.github.com>
2020-03-02 13:34:03 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,13 @@
 #Create s3 bucket
-provider = “aws” {
+terraform {
+  required_version = ">= 0.12.0"
+}
+provider “aws” {
   access_key = “${var.aws_access_key}”
   secret_key = “${var.aws_secret_key}”
   region = “${var.aws_region}” 
 }
 
-resource = “aws_s3” {
+resource “aws_s3” {
   bucket_name = “${var.aws_buckname}”
 } 
