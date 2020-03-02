75dfd1b3f20562d35e4bc373a826bb04c48ce426
itspurnima <61597739+itspurnima@users.noreply.github.com>
2020-03-02 12:58:01 +0000

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,10 @@
+#Create s3 bucket
+provider  “aws” {
+  access_key = “${var.aws_access_key}”
+  secret_key = “${var.aws_secret_key}”
+  region = “${var.aws_region}” 
+}
+
+resource “aws_s3” {
+  bucket_name = “${var.aws_buckname}”
+} 
